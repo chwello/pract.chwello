ec01b0a7d081ca68af84c86ca829f1000a6cb826
chelo.chel <122210462+chwello@users.noreply.github.com>
2023-03-27 22:37:10 +0800

    Update home_page.dart

--- home_page.dart
+++ home_page.dart
@@ -13,14 +13,14 @@ class HomePage extends StatelessWidget {
           children: [
             Padding(
               padding:
-                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
               child: Wrap(
                 spacing: 8,
                 children: [
                   ElevatedButton(
                     onPressed: () {},
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.redAccent,
+                      backgroundColor: const Color.fromARGB(255, 170, 0, 0),
                       foregroundColor: Colors.white,
                       shape: const StadiumBorder(),
                     ),
@@ -29,117 +29,131 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ),
-            GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) {
-                      return const DescriptionPage();
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 30.0, vertical: 5.0),
+              child: Wrap(
+                spacing: 40.0,
+                runSpacing: 10.0,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) {
+                            return const DescriptionPage();
+                          },
+                        ),
+                      );
                     },
-                  ),
-                );
-              },
-              child: Container(
-                width: double.infinity,
-                margin:
-                    const EdgeInsets.symmetric(horizontal: 20.0, vertical: 7.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.black12,
-                      spreadRadius: 2,
-                      blurRadius: 2,
-                      offset: Offset(2, 2),
-                    )
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    Image.asset('images/police.png'),
-                    const ListTile(
-                      title: Text('Police Station'),
-                      trailing: Icon(Icons.arrow_forward_ios_rounded),
+                    child: Container(
+                      width: 140.0,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black12,
+                            spreadRadius: 2,
+                            blurRadius: 2,
+                            offset: Offset(2, 2),
+                          )
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            'images/police.png',
+                            width: 60,
+                            height: 90,
+                          ),
+                          const ListTile(
+                            title: Text('Police Station'),
+                          ),
+                        ],
+                      ),
                     ),
-                  ],
-                ),
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              margin:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 7.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black12,
-                    spreadRadius: 2,
-                    blurRadius: 2,
-                    offset: Offset(2, 2),
-                  )
-                ],
-              ),
-              child: Column(
-                children: [
-                  Image.asset('images/fire.png'),
-                  const ListTile(
-                    title: Text('Fire Station'),
-                    trailing: Icon(Icons.arrow_forward_ios_rounded),
                   ),
-                ],
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              margin:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 7.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black12,
-                    spreadRadius: 2,
-                    blurRadius: 2,
-                    offset: Offset(2, 2),
-                  )
-                ],
-              ),
-              child: Column(
-                children: [
-                  Image.asset('images/hospital.png'),
-                  const ListTile(
-                    title: Text('Hospital'),
-                    trailing: Icon(Icons.arrow_forward_ios_rounded),
+                  Container(
+                    width: 140.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          spreadRadius: 2,
+                          blurRadius: 2,
+                          offset: Offset(2, 2),
+                        )
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'images/fire1.png',
+                          width: 40,
+                          height: 90,
+                        ),
+                        const ListTile(
+                          title: Text('Fire Station'),
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              margin:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 7.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black12,
-                    spreadRadius: 2,
-                    blurRadius: 2,
-                    offset: Offset(2, 2),
-                  )
-                ],
-              ),
-              child: Column(
-                children: [
-                  Image.asset('images/government.png'),
-                  const ListTile(
-                    title: Text('Government'),
-                    trailing: Icon(Icons.arrow_forward_ios_rounded),
+                  Container(
+                    width: 147.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          spreadRadius: 2,
+                          blurRadius: 2,
+                          offset: Offset(2, 2),
+                        )
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'images/hospital.png',
+                          width: 60,
+                          height: 90,
+                        ),
+                        const ListTile(
+                          title: Text('Hospital'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 150.0,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black12,
+                          spreadRadius: 2,
+                          blurRadius: 2,
+                          offset: Offset(2, 2),
+                        )
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'images/government.png',
+                          width: 60,
+                          height: 90,
+                        ),
+                        const ListTile(
+                          title: Text('Government'),
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
